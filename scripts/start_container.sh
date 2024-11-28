57550881d18a50a5053051940303c8106c2af7eb
Krishna Reddy <127050822+Krishna6899@users.noreply.github.com>
2024-11-28 13:53:11 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -6,4 +6,4 @@ docker pull krishna6899/simple-python-flask-app
 
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 krishna6899/simple-python-flask-app
+docker run -d -p 5000:8000 krishna6899/simple-python-flask-app
